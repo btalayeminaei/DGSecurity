4ff67fdfa0bebfb5c0e178bbecdc4ce2a047cfcc
Styopa Semenukha <semenukha@gmail.com>
2015-07-29 11:58:22 -0400

    Added MVP classes

---
+++ classes/presenters.php
@@ -0,0 +1,10 @@
+<?php
+namespace presenters;
+
+class Details {
+	protected $view;
+
+	function __construct($view) {
+		$this->view = $view;
+	}
+}
---
+++ classes/views.php
@@ -0,0 +1,17 @@
+<?php
+require_once("smarty3/Smarty.class.php");
+namespace views;
+
+interface IView {
+	function render($vars);
+}
+
+abstract class View implements IView {
+	function render($vars) {
+		$path = explode('\\', get_class($this));
+		$tpl = strtolower(end($path)) . '.tpl';
+		$smarty = new Smarty();
+		$smarty->assign($vars);
+		$smarty->display();
+	}
+}
