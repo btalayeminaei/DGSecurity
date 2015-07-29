9164aea8cd932e97fdbb4cb356c4f291d92a824b
Styopa Semenukha <semenukha@gmail.com>
2015-07-28 20:00:07 -0400

    Fixed syntax error

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 namespace security;
 spl_autoload_register( function ($class) {
 	$parts = explode('\\', strtolower($class));
-	array_unshift($parts, 'classes')
+	array_unshift($parts, 'classes');
 	$file = implode(DIRECTORY_SEPARATOR, $parts) . '.php';
 	if (is_file($file)) {
 		require_once($file);
