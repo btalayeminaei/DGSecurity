6ae4209f7cba360ad6923c57899d2ad5ea25e58e
Styopa Semenukha <ssemenukha@developmentgateway.org>
2015-08-14 17:37:53 -0400

    Designing login controller

--- classes/presenters.php
+++ classes/presenters.php
@@ -4,8 +4,11 @@ namespace presenters;
 class PresenterError extends \Exception { } 
 
 class SecurityError extends \Exception {
-	public function display() {
+	public function failLogin() {
 		http_response_code(403);
+	}
+
+	public function showLogin() {
 	}
 }
 
@@ -29,20 +32,45 @@ interface IPresenter {
 	public function run();
 }
 
-class Details implements IPresenter {
-	protected $action;
+abstract class Presenter implements IPresenter {
+	protected $get;
 
-	public function __construct($get) {
-		if (isset($get['action'])) {
-			$this->action = strtolower($get['action']);
-		}
+	function __construct($get) {
+		$this->get = $get;
 	}
 
+	protected function getAction() {
+		if (isset($this->get['action'])) {
+			$action = strtolower($this->get['action']);
+		} else {
+			$action = null;
+		}
+		return $action;
+}
+
+class Details extends Presenter implements IPresenter {
 	public function run() {
-		switch ($this->action) {
+		switch ($this->getAction()) {
 		default:
 			$view = new \views\Details();
 			$view->render($vars);
 		}
+	}
+}
+
+class Login implements IPresenter {
+	public function run() {
+		switch ($this->getAction()) {
+		}
+	}
+
+	public function redirect() {
+		http_response_code(302);
+		$scheme = isset($_SERVER['HTTPS']) ? 'https' : 'http';
+		$server = $_SERVER['SERVER_NAME'];
+		$uri = '/login';
+		$login_url = "$scheme://$server$uri";
+		header("Location: $login_url");
+		echo "Please log in at $login_url";
 	}
 }
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+ob_start();
+
 require_once('settings.php');
 require_once('autoload.php');
 
 $pres = \presenters\Factory::getPresenter($_GET);
 $pres->run();
+
+ob_end_flush();
